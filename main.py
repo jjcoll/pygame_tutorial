826dd57f220342eac2d66ef688e41b099061eaad
jjcoll <jjordicoll@gmail.com>
2023-10-16 19:02:25 +0200

    added text surface

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import pygame
 from sys import exit
 from surfaces import *
 
-
 pygame.init()
 width = 800
 height = 400
@@ -12,6 +11,12 @@ pygame.display.set_caption('Runner')
 
 clock = pygame.time.Clock()
 
+# create a font
+test_font = pygame.font.Font('font/Pixeltype.ttf', 50)  # font type and size
+text = 'My Game'
+anti_alias = False  # smooth text
+text_sur = test_font.render(text, anti_alias, 'Black')
+
 while True:
 
     for event in pygame.event.get():
@@ -19,9 +24,10 @@ while True:
             pygame.quit()
             exit()
 
+
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 300))
-
+    screen.blit(text_sur, (10, 10))
 
 
     pygame.display.update()
