974e7668c4159fa54fd07acdfc922cdc6a68ad17
jjcoll <jjordicoll@gmail.com>
2023-10-16 19:47:14 +0200

    basic snail animation

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ height = 400
 
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Runner')
-
 clock = pygame.time.Clock()
 
 # create a font
@@ -16,6 +15,10 @@ test_font = pygame.font.Font('font/Pixeltype.ttf', 50)  # font type and size
 text = 'My Game'
 anti_alias = False  # smooth text
 text_sur = test_font.render(text, anti_alias, 'Black')
+
+# import snail image
+snail_surface = pygame.image.load('graphics/snail/snail1.png')
+snail_x_pos = 600
 
 while True:
 
@@ -29,6 +32,12 @@ while True:
     screen.blit(ground_surface, (0, 300))
     screen.blit(text_sur, (10, 10))
 
+
+    screen.blit(snail_surface, (snail_x_pos, 270))
+
+    snail_x_pos -= 2  # make snail move to the left
+    if snail_x_pos < -100:
+        snail_x_pos = 800
 
     pygame.display.update()
     clock.tick(60)
